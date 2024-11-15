4890acddb7f7b6af819e63a95034e0f04d2a94a6
daviddavdavid <111596459+daviddavdavid@users.noreply.github.com>
2024-11-15 15:42:43 +0100

    Added a approx feature to the code

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -38,6 +38,19 @@ def tan(number):
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
+@cmd_group.command()
+@click.argument("epsilon", type=float)
+def approx(epsilon):
+    """Finds the largest x where the sin approximation for a given EPSILON does not hold anymore.
+
+    EPSILON is the number (of type float) where | sin(x) - x | must be <= EPSILON.
+    """
+    x = 0
+    while np.abs(np.sin(x) - x) <= epsilon:
+        x += 0.001
+    print(f"For an accuracy of {epsilon}, the small-angle approximation holds up to x = {round(x, 3)}.")
+
+
 
 if __name__ == "__main__":
     cmd_group()
