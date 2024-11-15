77315431ad680b9af18faae5cd8fc0c96a1ae19e
daviddavdavid <111596459+daviddavdavid@users.noreply.github.com>
2024-11-15 15:56:53 +0100

    Added better spacing

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -29,6 +29,7 @@ def sin(number):
     "-n",
     default=10,
 )
+
 def tan(number):
     """Runs the tan command for NUMBER amount of steps.
 
@@ -41,7 +42,7 @@ def tan(number):
 @cmd_group.command()
 @click.argument("epsilon", type=float)
 def approx(epsilon):
-    """Finds the largest x where the sin approximation for a given EPSILON does not hold anymore.
+    """Finds the largest x where the sin approximation for a given EPSILON does not hold anymore with an accuracy of 0.001.
 
     EPSILON is the number (of type float) where | sin(x) - x | must be <= EPSILON.
     """
@@ -49,8 +50,6 @@ def approx(epsilon):
     while np.abs(np.sin(x) - x) <= epsilon:
         x += 0.001
     print(f"For an accuracy of {epsilon}, the small-angle approximation holds up to x = {round(x, 3)}.")
-
-
 
 if __name__ == "__main__":
     cmd_group()
